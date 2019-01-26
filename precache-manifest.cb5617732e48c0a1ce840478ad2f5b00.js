41a7c08d757c7a4aad094bc55812f94131175023
vpaumen <vpaumen@gmail.com>
2019-01-25 23:31:33 -0500

    Updates

---
+++ precache-manifest.cb5617732e48c0a1ce840478ad2f5b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "db67f38eb20ad5cda8e4",
+    "url": "/clicky-game/static/js/main.db67f38e.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "db67f38eb20ad5cda8e4",
+    "url": "/clicky-game/static/css/main.2b2445b7.chunk.css"
+  },
+  {
+    "revision": "da15698bcf7733780fefca49dc218aa3",
+    "url": "/clicky-game/index.html"
+  }
+];
